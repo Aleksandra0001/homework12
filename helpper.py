0b408c488d0b590786cba95fdf4f2eab8df28306
Aleksandra0001 <104300297+Aleksandra0001@users.noreply.github.com>
2022-06-25 12:02:28 +0300

    Update helpper.py

--- helpper.py
+++ helpper.py
@@ -8,7 +8,7 @@ def check_age(value):
     if checking_age >= 100:
         raise Exception(
             f'Hey, grandpa! You are too old) Check if you have entered correct birthday date.')
-    elif current_date.year <= birthday.year:
+    elif current_date <= birthday:
         raise Exception(
             f'Hey, baby! You are too young) Check if you have entered correct birthday date.')
     else:
